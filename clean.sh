3ef278484b9131375e00ae62a1446498c72e70a2
Jason Chadwick <jchadwick100@gmail.com>
2022-05-19 12:49:41 -0400

    auto linux commit $date

--- clean.sh
+++ clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # clean up trash, downloads, etc
 
-find /home/jchad/Downloads/* -mtime +7 -exec rm -rf {} \;
-#find /home/jchad/Pictures/Screenshot* -atime +1 -exec rm {} \;
-trash-empty 7
-apt autoremove --purge --yes --allow-unauthenticated;
-apt clean --yes --allow-unauthenticated;
-journalctl --vacuum-time=7d;
+find /home/jchad/Downloads/* -atime +7 -exec rm -rf {} \; > /home/jchad/scripts/log/clean/clear-downloads.log 2>&1;
+find /home/jchad/Pictures/Screenshot* -mtime +7 -exec rm {} \; > /home/jchad/scripts/log/clean/delete-screenshots.log 2>&1;
+trash-empty 7 > /home/jchad/scripts/log/clean/trash-empty.log 2>&1;
+apt autoremove --purge --yes --allow-unauthenticated > /home/jchad/scripts/log/clean/apt-autoremove.log 2>&1;
+apt clean --yes --allow-unauthenticated > /home/jchad/scripts/log/clean/apt-clean.log 2>&1;
+journalctl --vacuum-time=7d > /home/jchad/scripts/log/clean/journal-vacuum.log 2>&1;
---
+++ obsidian-personal-sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/obsidian/personal/
+git pull
+git add -A
+git commit -m 'auto linux commit $date'
+git push
